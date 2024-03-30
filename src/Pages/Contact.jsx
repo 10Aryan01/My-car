91c29448f5f1e23087c571e7498d822851da9d68
Aryan <117532264+10Aryan01@users.noreply.github.com>
2024-03-31 01:40:37 +0530

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -60,7 +60,7 @@ function Contact() {
               <h2>Book a car by getting in touch with us</h2>
               <span>
                 <IconPhone width={40} height={40} />
-                <h3>(123) 456-7869</h3>
+                <h3>(+91) 6001012501</h3>
               </span>
             </div>
           </div>
